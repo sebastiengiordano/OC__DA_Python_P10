0e96987f23b1c9e6a8923c953a12df4e97d7b35b
Sébastien Giordano <sebastiengiordano@yahoo.fr>
2021-11-26 14:14:06 +0100

    adding rest_framework_simplejwt

--- project/settings.py
+++ project/settings.py
@@ -38,9 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'accounts.apps.AccountsConfig',
     'issue_tracking_system.apps.IssueTrackingSystemConfig',
 ]
+ 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',)
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
